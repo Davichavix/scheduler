0a9b7bea39532258bf86f005145e0811204c0e48
David Chan <dchan902@gmail.com>
2021-12-19 23:12:16 -0700

    add props for onClick and disabled for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,20 @@ import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
-
+ 
    if (props.confirm) {
      buttonClass += " button--confirm";
    }
-   return <button className={buttonClass}>{props.childeren}</button>;
-}
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return <button 
+      onClick={props.onClick} 
+      className={buttonClass}
+      disabled={props.disabled}
+      >
+         {props.children}
+      </button>;
+ }
