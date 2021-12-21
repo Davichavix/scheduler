895fdf38d305dcf69f8e0dba642fbb13109435e4
David Chan <dchan902@gmail.com>
2021-12-20 20:58:31 -0700

    add Confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,5 +153,12 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
             interviewers={interviewer}
             onEdit={action("onEdit")}
             onDelete={action("onDelete")}
+          />
+        ))
+        .add("Confirm", () => (
+          <Confirm
+            message="Delete the appointment?"
+            onConfirm={action("onConfirm")}
+            onCancel={action("onCancel")}
           />
         ))
