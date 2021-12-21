8fea25f9851e170e1e278bea9c9ccc6fadc80709
David Chan <dchan902@gmail.com>
2021-12-20 20:41:37 -0700

    create Appointment/Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,8 @@ import "components/Appointment/styles.scss"
 export default function Appointment(props) {
   return (
     <article className="appointment">
+      {!props.time && "No Appointment"}
+      {props.time && `Appointment at ${props.time}`}
     </article>
   );
 }
