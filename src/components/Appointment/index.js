257e9fb5b161a3a970dab42d633f04ef3664df49
David Chan <dchan902@gmail.com>
2021-12-20 20:09:43 -0700

    Add Appointment Story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+    </article>
   );
 }
