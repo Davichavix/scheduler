34e90965664a13ba68c68430eee660ff9841b3e4
David Chan <dchan902@gmail.com>
2021-12-20 22:27:51 -0700

    add cancel function to Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,16 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset()
+    return props.onCancel;
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -32,7 +42,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={cancel} danger>Cancel</Button>
           <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
