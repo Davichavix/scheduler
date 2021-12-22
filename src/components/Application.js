07cea23ddf75ed6d0cc5cf98019752a68c5831d3
David Chan <dchan902@gmail.com>
2021-12-21 23:21:11 -0700

    Make axios get call to API/Days

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,27 +46,16 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
+  
+  const [days, setDays] = useState([]);
 
-  const [day, setDay] = useState('Monday');
+  useEffect(() => {
+    const URL = `http://localhost:8001/api/days`
+    axios.get(URL).then(response => {
+      setDays([...response.data])
+    })
+  }, [])
 
   const apptData = Object.values(appointments).map((appointment) => {
     return (
@@ -89,8 +78,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
         <DayList
           days={days}
-          value={day}
-          onChange={setDay}
+          value={days}
+          onChange={setDays}
         />
       </nav>
       <img
