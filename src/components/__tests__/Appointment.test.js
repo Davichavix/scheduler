f06f3bd704481c6d10d1fbe1c4927f1714becd87
David Chan <dchan902@gmail.com>
2022-03-19 22:17:26 -0600

    add Form test.js

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render } from "@testing-library/react";
 
-import Application from "components/Application";
+import Appointment from "components/Application";
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
